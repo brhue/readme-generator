68be63c5f95bf63058dffa356f8cf9147e04162c
Bradley Donahue <donahue.bradley@gmail.com>
2021-01-09 13:10:03 -0500

    Implement saving generated readme to file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -52,13 +54,20 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Saved to ${fileName}`);
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {
-  inquirer.prompt(questions).then((response) => {
-    console.log(response);
-  });
+async function init() {
+  const response = await inquirer.prompt(questions);
+  writeToFile('example-README.md', generateMarkdown(response));
 }
 
 // Function call to initialize app
