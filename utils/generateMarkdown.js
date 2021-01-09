846aaa3823c9e2248b959fe4e8b4d122b4fbb0b2
Bradley Donahue <donahue.bradley@gmail.com>
2021-01-09 12:08:03 -0500

    Implement markdown more complete markdown generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'ISC':
+      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+    case 'ISC':
+      return '[ISC](https://opensource.org/licenses/ISC)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,8 +30,42 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
+${renderLicenseBadge(data.projectLicense)}
+## Description
+${data.projectDescription}
 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+\`\`\`
+${data.projectInstall}
+\`\`\`
+
+## Usage
+\`\`\`
+${data.projectUsage}
+\`\`\`
+
+## License
+${renderLicenseLink(data.projectLicense)}
+
+## Contributing
+${data.projectContrib}
+
+## Tests
+\`\`\`
+${data.projectTesting}
+\`\`\`
+
+## Questions
+If you have any questions you can reach out to me on [Github](https://github.com/${data.username}) or shoot me an [email](mailto:${data.email}).
 `;
 }
 
